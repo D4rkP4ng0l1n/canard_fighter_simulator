1abd479ea71f1379b4228883685c4e881d75951b
Clément Rieuneau <clem.rieuneau@gmail.com>
2025-03-27 16:18:56 +0100

    [CanardVent] >> Capacite Speciale du type Vent

--- src/main/java/combat/CapaciteSpecialeVent.java
+++ src/main/java/combat/CapaciteSpecialeVent.java
@@ -6,10 +6,6 @@ public class CapaciteSpecialeVent implements CapaciteSpeciale {
 
     @Override
     public void activer(Canard canardCible) {
-        if (canardCible.getPointsAttaque() < 10) {
-            throw new IllegalArgumentException(
-                    "Le canard cible n'a pas assez de points d'attaque pour activer la capacité spéciale !");
-        }
-        // Effet de la capacite
+        canardCible.subirDegats(40);
     }
 }
--- src/main/java/modele/CanardVent.java
+++ src/main/java/modele/CanardVent.java
@@ -8,4 +8,13 @@ public class CanardVent extends Canard {
         super(nom, TypeCanard.VENT, pv, pa, new CapaciteSpecialeVent());
     }
 
+    public void activerCapaciteSpeciale(Canard canardCible) {
+        if (this.getPointsAttaque() < 15) {
+            throw new IllegalArgumentException(
+                    "Le canard n'a pas assez de points d'attaque pour activer la capacité spéciale !");
+        }
+        this.getCapaciteSpeciale().activer(canardCible);
+        this.setPointsAttaque(this.getPointsAttaque() - 15);
+    }
+
 }
